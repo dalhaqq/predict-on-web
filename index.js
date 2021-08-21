c2f80f5b2a860009a759e8247783ce912127ca3d
Abdalhaqq MS <abdalhaqqm@gmail.com>
2021-08-22 02:26:09 +0700

    Add prediction functionality

--- index.js
+++ index.js
@@ -1,48 +1,35 @@
-let rawData = `
-    LAKI-LAKI, RENDAH, 1, YA
-    LAKI-LAKI, TINGGI, 2, TIDAK
-    LAKI-LAKI, TINGGI, 1, YA
-    PEREMPUAN, RENDAH, 1, TIDAK
-    LAKI-LAKI, RENDAH, 1, TIDAK
-    LAKI-LAKI, TINGGI, 1, TIDAK
-    PEREMPUAN, RENDAH, 2, TIDAK
-    PEREMPUAN, RENDAH, 2, YA
-    PEREMPUAN, TINGGI, 1, TIDAK
-    PEREMPUAN, RENDAH, 1, TIDAK
-    LAKI-LAKI, RENDAH, 2, YA
-    PEREMPUAN, RENDAH, 1, TIDAK
-    PEREMPUAN, RENDAH, 1, TIDAK
-    LAKI-LAKI, RENDAH, 2, TIDAK
-    LAKI-LAKI, TINGGI, 2, TIDAK
-    PEREMPUAN, RENDAH, 1, TIDAK
-    PEREMPUAN, RENDAH, 2, TIDAK
-    PEREMPUAN, TINGGI, 1, TIDAK
-    LAKI-LAKI, TINGGI, 1, TIDAK
-    LAKI-LAKI, RENDAH, 2, YA
-`;
+let rawData, dataSet, trainData, labels;
 
-let dataSet, X, Y;
-
-function init() {
+function reset() {
     dataSet = [];
-    X = [];
-    Y = [];
+    trainData = [];
+    labels = [];
+    params = [];
 }
 
 function generateForm() {
     let form = document.getElementById('prediction-box');
+    while(form.hasChildNodes()) {
+        form.removeChild(form.lastChild);
+    }
     for (let i = 0; i < dataSet.length - 1; i++) {
+        let label = document.createElement('label');
+        label.innerHTML = params[i];
+        label.setAttribute('for', 'val' + i);
         let select = document.createElement('select');
         select.setAttribute('id', 'val' + i);
         select.setAttribute('name', 'val' + i);
-        let vals = dataSet[i].values(); 
+        let vals = dataSet[i].values();
         for (let val = vals.next(); val.done === false; val = vals.next()) {
             let option = document.createElement('option');
             option.setAttribute('value', val.value);
             option.innerHTML = val.value;
             select.appendChild(option);
         }
+        let br = document.createElement('br');
+        form.appendChild(label);
         form.appendChild(select);
+        form.appendChild(br);
     }
     let submit = document.createElement('button');
     submit.setAttribute('onclick', 'predict()');
@@ -51,8 +38,11 @@ function generateForm() {
 }
 
 function processData() {
-    for (let i = 0, data = rawData.split('\n');
-        i < data.length; i++) {
+    reset();
+    rawData = document.getElementById('data').value;
+    let data = rawData.trim().split('\n');
+    params = data.shift().split(',');
+    for (let i = 0; i < data.length; i++) {
         if (data[i] === '') {
             data.splice(i, 1);
             i--;
@@ -66,14 +56,58 @@ function processData() {
             data[i][j] = data[i][j].trim();
             dataSet[j].add(data[i][j]);
         }
-        Y.push(data[i].pop());
-        X.push(data[i]);
+        trainData.push(data[i]);
+        if (!labels.includes(data[i][data[i].length - 1])) {
+            labels.push(data[i][data[i].length - 1]);
+        }
     }
     generateForm();
 }
 
-function predict() {
-
+function count(arr, el) {
+    let counter = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == el) {
+            counter++;
+        }
+    }
+    return counter;
 }
 
-init();
+function predict() {
+    let inputData = [];
+    let counts = {};
+    let Y = [];
+    for (let i = 0; i < trainData.length; i++) {
+        Y.push(trainData[i][trainData[i].length - 1]);
+    }
+    for (let i = 0; i < dataSet.length - 1; i++) {
+        let select = document.getElementById("val" + i);
+        inputData.push(select.value);
+    }
+    labels.forEach(label => {
+        counts[label] = count(Y, label)/Y.length;
+    });
+    for (let i = 0; i < inputData.length; i++) {
+        labels.forEach(label => {
+            let XCount = 0;
+            for (let j = 0; j < trainData.length; j++) {
+                if (inputData[i] == trainData[j][i] && Y[j] == label) {
+                    XCount++;
+                }
+            }
+            counts[label] *= XCount/count(Y, label);
+        });
+    }
+    let max = 0;
+    let maxLabel = 'Unpredictable';
+    for (let label in counts) {
+        if (counts[label] > max) {
+            max = counts[label];
+            maxLabel = label;
+        }
+    }
+    let result = document.getElementById('result');
+    result.innerHTML = 'Result: ' + maxLabel + '<br>Score: ' + max;
+    console.log(counts);
+}
